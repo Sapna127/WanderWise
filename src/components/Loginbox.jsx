0badead0e8ac90c3fb2dea8a63230f8aa5e96b03
Sapna Kul <kulsapna2210002@gmail.com>
2025-03-24 03:45:07 +0530

    refactor: hide loginbox in small screens

--- src/components/Loginbox.jsx
+++ src/components/Loginbox.jsx
@@ -1,12 +1,11 @@
 export default function Loginbox() {
-    return (
-      <div className="fixed top-10 right-4 bg-white shadow-lg border z-10 border-gray-200 p-4 rounded-lg w-100 flex items-start">
-        <span className="text-xl">🔐</span>
-        <div className="ml-2">
-          <h2 className="font-bold text-black">Unlock the best of wonderise</h2>
-          <p className="text-gray-600 text-sm">Save itineraries, invite collaborators etc</p>
-        </div>
+  return (
+    <div className="hidden md:flex fixed top-10 right-4 bg-white shadow-lg border z-10 border-gray-200 p-4 rounded-lg w-100 items-start">
+      <span className="text-xl">🔐</span>
+      <div className="ml-2">
+        <h2 className="font-bold text-black">Unlock the best of wonderise</h2>
+        <p className="text-gray-600 text-sm">Save itineraries, invite collaborators etc</p>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
